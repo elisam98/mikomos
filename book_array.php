ddef8dd466c2c85a8e87faf36e506caddad47ba1
elisam98 <cornbeefonrye@hotmail.com>
2013-07-23 08:37:39 -0400

    pre-index build

--- book_array.php
+++ book_array.php
@@ -4,37 +4,24 @@ error_reporting(E_ALL | E_STRICT);
 
 $continue = "";
 $i = 1;
-$fullResults = "";
 
 do 
 {
-	$template = file_get_contents("book_template.html");
-
 	$xml = simplexml_load_file("http://www.mikomos.com/w/api.php?action=query&format=xml&generator=allpages&prop=revisions&rvprop=content&gaplimit=max&gapcontinue=".$continue);
 	if ($xml->{'query-continue'}) {
 		$continue = ((string)$xml->{'query-continue'}[0]->allpages['gapcontinue']);
 	}
 	
-$pages = ($xml->query->pages);
+	$pages = ($xml->query->pages);
 
 	foreach ($pages->page as $page) {
 
-		$title = (string)$page['title'];
-
 		$content = (string)$page->revisions->rev;
 
-		$mb = "{{Makombox";
+//		$mb = "{{Makombox";
 		$makom = strpos($content, "{{Makombox");
 
 		if ($makom !== false) {
-//			utf8_decode($content);
-			$template = file_get_contents("book_template.html");
-			$template = str_replace("{{title}}", $title, $template);
-			
-//			echo "<strong>";
-//			echo $title;
-//			echo "</strong>";
-//			echo "<br>";
 
 			$content = str_replace("\n", "", $content);
 			$content = str_replace("[", "", $content);
@@ -44,12 +31,14 @@ $pages = ($xml->query->pages);
 			$content = str_replace("}}", "", $content);
 			$content = str_replace("|", "&", $content);
 
+			$title = (string)$page['title'];
+
 			$address="";$city="";$state="";$zip="";$country="";$phone="";
 			$neighborhood="";$price="";$web_page="";$basic_description = "";
 			$activity_type="";$tips="";$hours="";$directions="";
 			$cuisine="";$dairy_or_meat="";$hashgacha="";
 			$additional_kashrus="";$menu="";$lounge_type="";
-			$museum_type="";$park_type="";$shopping_type="";$image="";
+			$museum_type="";$park_type="";$shopping_type="";
 
 			parse_str($content);
 			if (!$country && $city) {
@@ -73,150 +62,152 @@ $pages = ($xml->query->pages);
 				$neighborhood = "";;
 			}
 // // // //
-
+			$array[$i]['title'] = $title;
+			
 			if ($address) {        
-				$array[$country][$state][$neighborhood][$title]['address'] = $address;
+				$array[$i]['address'] = $address;
 //				++$i;
 //				echo "Address: ".$address;
 //				echo "<br>";
 			}
 			if ($city) {
-				$array[$country][$state][$neighborhood][$title]['city'] = $city;
+				$array[$i]['city'] = $city;
 //				echo "City: ".$city;
 //				echo "<br>";
 			}
 			if ($state) {
-				$array[$country][$state][$neighborhood][$title]['state'] = $state;
+				$array[$i]['state'] = $state;
 //				echo "State: ".$state;
 //				echo "<br>";
 			}
 			if ($zip) {
-				$array[$country][$state][$neighborhood][$title]['zip'] = $zip;
+				$array[$i]['zip'] = $zip;
 //				echo "Zip: ".$zip;
 //				echo "<br>";
 			}
 			if ($country) {
-				$array[$country][$state][$neighborhood][$title]['country'] = $country;
+				$array[$i]['country'] = $country;
 //				echo "Country: ".$country;
 //				echo "<br>";
 			}
 			if ($phone) {
-				$array[$country][$state][$neighborhood][$title]['phone'] = $phone;
+				$array[$i]['phone'] = $phone;
 //				echo "Phone: ".$phone;
 //				echo "<br>";
 			}
 			if ($neighborhood) {
-				$array[$country][$state][$neighborhood][$title]['neighborhood'] = $neighborhood;
+				$array[$i]['neighborhood'] = $neighborhood;
 //				echo "Neighborhood: ".$neighborhood;
 //				echo "<br>";
 			}
 			if ($price) {
-				$array[$country][$state][$neighborhood][$title]['price'] = $price;
+				$array[$i]['price'] = $price;
 //				echo "Price: ".$price;
 //				echo "<br>";
 			}
-			if ($web_page) {
-//				echo "Website: <a href='".$web_page."'>".$web_page."</a>";
-//				echo "<br>";
+/* 			if ($web_page) {
+				echo "Website: <a href='".$web_page."'>".$web_page."</a>";
+				echo "<br>";
 			}
-			if ($activity_type) {
-				$array[$country][$state][$neighborhood][$title]['activity_type'] = $activity_type;
+ */			if ($activity_type) {
+				$array[$i]['activity_type'] = $activity_type;
 //			echo "<strong>";
 //				echo "Activity: ".$activity_type;
 //				echo "<br>";
 //			echo "</strong>";
 			}
 			if ($lounge_type) {
-				$array[$country][$state][$neighborhood][$title]['lounge_type'] = $activity_type;
+				$array[$i]['lounge_type'] = $activity_type;
 //			echo "<strong>";
 //				echo "Lounge Type: ".$lounge_type;
 //				echo "<br>";
 //			echo "</strong>";
 			}
 			if ($museum_type) {
-				$array[$country][$state][$neighborhood][$title]['museum_type'] = $museum_type;
+				$array[$i]['museum_type'] = $museum_type;
 //			echo "<strong>";
 //				echo "Museum Type: ".$museum_type;
 //				echo "<br>";
 //			echo "</strong>";
 			}
 			if ($park_type) {
-				$array[$country][$state][$neighborhood][$title]['park_type'] = $park_type;
+				$array[$i]['park_type'] = $park_type;
 //			echo "<strong>";
 //				echo "Park Type: ".$park_type;
 //				echo "<br>";
 //			echo "</strong>";
 			}
 			if ($shopping_type) {
-				$array[$country][$state][$neighborhood][$title]['shopping_type'] = $shopping_type;
+				$array[$i]['shopping_type'] = $shopping_type;
 //			echo "<strong>";
 //				echo "Shopping Type: ".$shopping_type;
 //				echo "<br>";
 //			echo "</strong>";
 			}
 			if ($cuisine) {
-				$array[$country][$state][$neighborhood][$title]['cuisine'] = $cuisine;
+				$array[$i]['cuisine'] = $cuisine;
 //			echo "<strong>";
 //				echo "Cuisine: ".$cuisine;
 //				echo "<br>";
 //			echo "</strong>";
 			}
 			if ($basic_description) {
-				$array[$country][$state][$neighborhood][$title]['basic_description'] = $basic_description;
+				$array[$i]['basic_description'] = $basic_description;
 //				echo "Desciption: ".$basic_description;
 //				echo "<br>";
 			}
 			if ($tips) {
-				$array[$country][$state][$neighborhood][$title]['tips'] = $tips;
+				$array[$i]['tips'] = $tips;
 //				echo "Tips: ".$tips;
 //				echo "<br>";
 			}
 			if ($dairy_or_meat) {
-				$array[$country][$state][$neighborhood][$title]['dairy_or_meat'] = $dairy_or_meat;
+				$array[$i]['dairy_or_meat'] = $dairy_or_meat;
 //				echo "Dairy or Meat? ".$dairy_or_meat;
 //				echo "<br>";
 			}
 			if ($hashgacha) {
-				$array[$country][$state][$neighborhood][$title]['hashgacha'] = $hashgacha;
+				$array[$i]['hashgacha'] = $hashgacha;
 //				echo "Hashgacha: ".$hashgacha;
 //				echo "<br>";
 			}
 			if ($additional_kashrus) {
-				$array[$country][$state][$neighborhood][$title]['additional_kashrus'] = $additional_kashrus;
+				$array[$i]['additional_kashrus'] = $additional_kashrus;
 //				echo "additional_kashrus: ".$additional_kashrus;
 //				echo "<br>";
 			}
 			if ($menu) {
-				$array[$country][$state][$neighborhood][$title]['menu'] = $menu;
+				$array[$i]['menu'] = $menu;
 //				echo "Menu: <a href='".$menu."'>".$menu."</a>";
 //				echo "<br>";
 			}
 			if ($hours) {
-				$array[$country][$state][$neighborhood][$title]['hours'] = $hours;
+				$array[$i]['hours'] = $hours;
 //				echo "Hours: ".$hours;
 //				echo "<br>";
 			}
 			if ($directions) {
-				$array[$country][$state][$neighborhood][$title]['directions'] = $directions;
+				$array[$i]['directions'] = $directions;
 //				echo "Directions: ".$directions;
 //				echo "<br>";
 			}
-			if ($image) {
-				if (strpos($image, "http://")) {
-					echo "<img src='".$image."' />";
-				}
-				else {
-					echo "<img src='http://www.mikomos.com/w/images/b/b4/".$image."' />";
-				}
-			}
 			++$i;
-			$fullResults .= $template;
 		}
 	}
 }
 while ($xml->{'query-continue'});
 
-// print_r($array);
+foreach ($array as $key => $row) {
+	// if ($row['state']) {
+		$countrySort[$key] = $row['country'];
+		$stateSort[$key] = $row['state'];
+		$neighborhoodSort[$key] = $row['neighborhood'];
+		$titleSort[$key] = $row['title'];
+	// }
+}
+
+array_multisort($countrySort, SORT_ASC, $stateSort, SORT_ASC, $neighborhoodSort, SORT_ASC, $titleSort, SORT_ASC, $array);
+print_r($array);
 // print_r ($fullResults);
 
 ?>
--- book_sorted.php
+++ book_sorted.php
@@ -31,25 +31,178 @@
 	</head>
 	<body>
 <?php
-ini_set('display_errors', 'On');
-error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 'Off');
+// error_reporting(E_ALL | E_STRICT);
 
 $continue = "";
 $i = 1;
-$fullResults = "";
 
 do 
 {
-//	$template = file_get_contents("book_template.html");
-
 	$xml = simplexml_load_file("http://www.mikomos.com/w/api.php?action=query&format=xml&generator=allpages&prop=revisions&rvprop=content&gaplimit=max&gapcontinue=".$continue);
 	if ($xml->{'query-continue'}) {
 		$continue = ((string)$xml->{'query-continue'}[0]->allpages['gapcontinue']);
 	}
 	
-$pages = ($xml->query->pages);
+	$pages = ($xml->query->pages);
 
 	foreach ($pages->page as $page) {
+
+		$content = (string)$page->revisions->rev;
+
+		$makom = strpos($content, "{{Makombox");
+
+		if ($makom !== false) {
+
+			$content = str_replace("\n", "", $content);
+			$content = str_replace("[", "", $content);
+			$content = str_replace("]", "", $content);
+			$content = str_replace("{{Makombox All", "", $content);
+			$content = str_replace("{{Tags", "", $content);
+			$content = str_replace("}}", "", $content);
+			$content = str_replace("|", "&", $content);
+//			$content = str_replace("*", "<br />&bull;", $content);
+
+			$title="";
+			$address="";$city="";$state="";$zip="";$country="";$phone="";
+			$neighborhood="";$price="";$web_page="";$basic_description = "";
+			$activity_type="";$tips="";$hours="";$directions="";
+			$cuisine="";$dairy_or_meat="";$hashgacha="";
+			$additional_kashrus="";$menu="";$lounge_type="";
+			$museum_type="";$park_type="";$shopping_type="";
+
+			parse_str($content);
+			if (!$country && $city) {
+						$geoAddr = str_replace(" ", "+", $address);
+						$geoCity = str_replace(" ", "+", $city);
+
+						$geoURL = "http://www.datasciencetoolkit.org/street2coordinates/".$geoAddr.",+".$geoCity.",+".$state."+".$zip;
+						$geoJSON = file_get_contents($geoURL);
+						$geo = json_decode($geoJSON);
+						foreach ($geo as $key => $value) {
+							if ($value && $value != "null") {
+								$country = $value->{"country_code3"};
+							}
+						}
+			}
+			if ($country) {
+				$country = ucwords($country);
+			}
+			if ($state != "NY" && $state != "NJ") {
+				$neighborhood = "";
+			}
+// // // //
+			$title = (string)$page['title'];
+			$array[$i]['title'] = $title;
+			
+			if ($address) {        
+				$array[$i]['address'] = $address;
+			}
+			if ($city) {
+				$array[$i]['city'] = $city;
+			}
+			if ($state) {
+				$array[$i]['state'] = $state;
+			}
+			if ($zip) {
+				$array[$i]['zip'] = $zip;
+			}
+			if ($country) {
+				$array[$i]['country'] = $country;
+			}
+			if ($phone) {
+				$array[$i]['phone'] = $phone;
+			}
+			if ($neighborhood) {
+				$array[$i]['neighborhood'] = $neighborhood;
+			}
+			if ($price) {
+				$array[$i]['price'] = $price;
+			}
+			if ($activity_type) {
+				$array[$i]['activity_type'] = $activity_type;
+			}
+			if ($lounge_type) {
+				$array[$i]['lounge_type'] = $activity_type;
+			}
+			if ($museum_type) {
+				$array[$i]['museum_type'] = $museum_type;
+			}
+			if ($park_type) {
+				$array[$i]['park_type'] = $park_type;
+			}
+			if ($shopping_type) {
+				$array[$i]['shopping_type'] = $shopping_type;
+			}
+			if ($cuisine) {
+				$array[$i]['cuisine'] = $cuisine;
+			}
+			if ($basic_description) {
+				$array[$i]['basic_description'] = $basic_description;
+			}
+			if ($tips) {
+				$array[$i]['tips'] = $tips;
+			}
+			if ($dairy_or_meat) {
+				$array[$i]['dairy_or_meat'] = $dairy_or_meat;
+			}
+			if ($hashgacha) {
+				$array[$i]['hashgacha'] = $hashgacha;
+			}
+			if ($additional_kashrus) {
+				$array[$i]['additional_kashrus'] = $additional_kashrus;
+			}
+			if ($menu) {
+				$array[$i]['menu'] = $menu;
+			}
+			if ($hours) {
+				$array[$i]['hours'] = $hours;
+			}
+			if ($directions) {
+				$array[$i]['directions'] = $directions;
+			}
+			++$i;
+		}
+	}
+}
+while ($xml->{'query-continue'});
+
+foreach ($array as $key => $row) {
+		$countrySort[$key] = $row['country'];
+		$stateSort[$key] = $row['state'];
+		$neighborhoodSort[$key] = $row['neighborhood'];
+		$titleSort[$key] = $row['title'];
+}
+
+array_multisort($countrySort, SORT_ASC, $stateSort, SORT_ASC, $neighborhoodSort, SORT_ASC, $titleSort, SORT_ASC, $array);
+
+foreach ($array as $key => $value) {
+
+	$title = $value['title'];
+	$address = $value['address'];
+	$city = $value['city'];
+	$state = $value['state'];
+	$zip = $value['zip'];
+	$country = $value['country'];
+	$phone = $value['phone'];
+	$neighborhood = $value['neighborhood'];
+	$price = $value['price'];
+	$basic_description = $value['basic_description'];
+	$activity_type = $value['activity_type'];
+	$tips = $value['tips'];
+	$hours = $value['hours'];
+	$directions = $value['directions'];
+	$cuisine = $value['cuisine'];
+	$dairy_or_meat = $value['dairy_or_meat'];
+	$hashgacha = $value['hashgacha'];
+	$additional_kashrus = $value['additional_kashrus'];
+	$lounge_type = $value['lounge_type'];
+	$museum_type = $value['museum_type'];
+	$park_type = $value['park_type'];
+	$shopping_type = $value['shopping_type'];
+	
+//	echo $country." -> ".$state." -> ".$neighborhood.": ".$title;
+//	echo "<br />";
 
 	$template = <<<EOF
 		<div class="header">
@@ -143,62 +296,17 @@ $pages = ($xml->query->pages);
 		</div>
 EOF;
 
-		$title = (string)$page['title'];
-
-		$content = (string)$page->revisions->rev;
-
-		$mb = "{{Makombox";
-		$makom = strpos($content, "{{Makombox");
-
-		if ($makom !== false) {
-
-//			$template = file_get_contents("book_template.html");
-			$template = str_replace("{{title}}", $title, $template);
-			
-			$content = str_replace("\n", "", $content);
-			$content = str_replace("[", "", $content);
-			$content = str_replace("]", "", $content);
-			$content = str_replace("{{Makombox All", "", $content);
-			$content = str_replace("{{Tags", "", $content);
-			$content = str_replace("}}", "", $content);
-			$content = str_replace("|", "&", $content);
-
-			$address="";$city="";$state="";$zip="";$country="";$phone="";
-			$neighborhood="";$price="";$web_page="";$basic_description = "";
-			$activity_type="";$tips="";$hours="";$directions="";
-			$cuisine="";$dairy_or_meat="";$hashgacha="";
-			$additional_kashrus="";$menu="";$lounge_type="";
-			$museum_type="";$park_type="";$shopping_type="";
-
-			parse_str($content);
-			if (!$country && $city) {
-						$geoAddr = str_replace(" ", "+", $address);
-						$geoCity = str_replace(" ", "+", $city);
-
-						$geoURL = "http://www.datasciencetoolkit.org/street2coordinates/".$geoAddr.",+".$geoCity.",+".$state."+".$zip;
-
-						$geoJSON = file_get_contents($geoURL);
-						$geo = json_decode($geoJSON);
-						foreach ($geo as $key => $value) {
-							if ($value && $value != "null") {
-								$country = $value->{"country_code3"};
-							}
-						}
-			}
-			if ($country) {
-				$country = ucwords($country);
-			}
-			if ($state != "NY" && $state != "NJ") {
-				$neighborhood = "";;
-			}
-
-        $template = str_replace("{{#}}", $i, $template);
+        $template = str_replace("{{#}}", $key+1, $template);
+        $template = str_replace("{{title}}", $title, $template);
 
 			if ($address) {        
 				$template = str_replace("{{address}}", $address, $template);
 			}
 			else {
-				$template = str_replace("<li> &bull; <span class=\"address\">{{address}}</span></li>", "", $template);
+//				$template = str_replace("<li> &bull; <span class=\"address\">{{address}}</span></li>", "", $template);
+				$template = str_replace("<span class=\"address\">{{address}}</span></li>
+					<li> &bull; 
+						", "", $template);
 			}
 			if ($city) {
 				$template = str_replace("{{city}}", $city, $template);
@@ -363,15 +471,11 @@ EOF;
 							</div>
 						</div>", "", $template);
 			}
-			++$i;
+//			++$i;
 //			$template .= "<div style=\"page-break-before: always\"> </div>";
 			print_r ($template."<div style=\"page-break-before: always\"> </div>");
-		}
-	}
+
 }
-while ($xml->{'query-continue'});
-
-
 ?>
 	</body>
 </html>
